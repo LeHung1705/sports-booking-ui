2532cdff135e0c3387d36f8498c74c57e3b77afa
HaNhutQuang <nhutquang100602@gmail.com>
2025-12-01 21:37:54 +0700

    fix: improve open directions logic with Google Maps fallback

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 // Thay IP máy bạn
-const API_BASE_URL = "http://192.168.1.14:8080/api/v1"; 
+const API_BASE_URL = "http://192.168.1.9:8080/api/v1"; 
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
